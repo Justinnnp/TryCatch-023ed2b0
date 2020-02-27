392bd4b44b143c0c859484fb15994f6366db85b5
Justin Plein <2099449@talnet.nl>
2020-02-27 11:44:25 +0100

    Uitwerking van de exercise

---
+++ my_error.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error's</title>
+</head>
+<body>
+    <?php
+
+    $rts = rand(0,10);
+    function countDown() {
+        echo ($rts);
+    }
+    ?>
+</body>
+</html>
